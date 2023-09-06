6a46c5006332d3254af61fd75f2bf7419f814386
Damiano Donati <damiano.donati@gmail.com>
2023-09-06 21:58:52 +0200

    update version string

--- cmd/gom/cmd/version.go
+++ cmd/gom/cmd/version.go
@@ -1,11 +1,10 @@
 package cmd
 
 import (
-	"context"
 	"errors"
 	"fmt"
-	"io"
 	"runtime/debug"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
@@ -16,7 +15,7 @@ var versionCmd = &cobra.Command{
 	Short: "returns the version of the program",
 	Long:  `returns the version of the program`,
 	RunE: func(cmd *cobra.Command, args []string) error {
-		return versionImpl.version(cmd.Context(), args, cmd.OutOrStdout(), cmd.OutOrStderr())
+		return versionImpl.version()
 	},
 }
 
@@ -29,13 +28,43 @@ var (
 	ErrReadingBuildInfo = errors.New("error reading build info")
 )
 
-func (r *versionImplConfig) version(ctx context.Context, args []string, stdout, stderr io.Writer) error {
-	bi, ok := debug.ReadBuildInfo()
-	if !ok {
-		return ErrReadingBuildInfo
-	}
-
-	fmt.Println(bi.Main.Version)
+func (r *versionImplConfig) version() error {
+	fmt.Println(readVersion())
 
 	return nil
+}
+
+func readVersionParts() (revision string, modified, ok bool) {
+	info, ok := debug.ReadBuildInfo()
+	if !ok {
+		return "", false, false
+	}
+	settings := make(map[string]string)
+	for _, s := range info.Settings {
+		settings[s.Key] = s.Value
+	}
+	// When built from a local VCS directory, we can use vcs.revision directly.
+	if rev, ok := settings["vcs.revision"]; ok {
+		return rev, settings["vcs.modified"] == "true", true
+	}
+	// When built as a Go module (not from a local VCS directory),
+	// info.Main.Version is something like v0.0.0-20230107144322-7a5757f46310.
+	v := info.Main.Version // for convenience
+	if idx := strings.LastIndexByte(v, '-'); idx > -1 {
+		return v[idx+1:], false, true
+	}
+	return "<BUG>", false, false
+}
+
+func readVersion() string {
+	revision, modified, ok := readVersionParts()
+	if !ok {
+		return "<unknown>"
+	}
+	modifiedSuffix := ""
+	if modified {
+		modifiedSuffix = " (modified)"
+	}
+
+	return revision + modifiedSuffix
 }
